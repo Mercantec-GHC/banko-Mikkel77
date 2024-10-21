91b967a98d0073b79da1955197fe1b308d7aa9ca
Mikkel77 <Mikkelskovhansen70@gmail.com>
2024-10-21 14:51:27 +0200

    test

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace BankoCheater
+﻿using System;
+using System.Collections.Generic;
+
+Console.WriteLine("Enter the amount of plates (max 5)");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string userInput = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+// Creating a dictionary
+Dictionary<int, int> iplate = new Dictionary<int, int>();
+
+int userPlates;
+if (int.TryParse(userInput, out userPlates))
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    if (userPlates >= 1 && userPlates <= 5)
+
+        for (int id = 1; id <= 5; id++)
         {
-            Console.WriteLine("Hello, World!");
+            if (id == userPlates)
+            {
+                Console.WriteLine($"du har valgt {userPlates} plader");
+                //WIP
+                iplate.Add(id, userPlates);
+                foreach (KeyValuePair<int, int> ele1 in iplate)
+                {
+                    Console.WriteLine("{0} and {1}",
+                              ele1.Key, ele1.Value);
+                }
+                Console.WriteLine();
+            }
         }
+    else
+    {
+        Console.WriteLine("something went wrong restart the program");
     }
 }
