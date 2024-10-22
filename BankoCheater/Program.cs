849292f05e489a501eebd8fc68f99896179c8c24
Mikkel77 <Mikkelskovhansen70@gmail.com>
2024-10-22 14:58:25 +0200

    banko project

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -1,16 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿while (true)
+{
+    Console.WriteLine("Skriv 15 tal fra pladen ind i format: første række og så den under og så den under, efter det skriv 'exit' for at afslutte indtastningen.");
+    var bankoPlates = Console.ReadLine();
 
-Console.WriteLine("Enter the amount of plates (max 5)");
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-string userInput = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+    if (bankoPlates?.ToLower() == "exit")
+    {
+        Console.WriteLine("Plader gemt");
+        break;  // Exit the while loop if "exit" is typed
+    }
 
+    // Check the first 5, 10, and 15 numbers in the plate
+    for (int i = 1; i <= 15; i++)
+    {
+        if (i == 5)
+        {
+            Console.WriteLine("Test");
+        }
+        else if (i == 10)
+        {
+            Console.WriteLine("Test2");
+        }
+        else if (i == 15)
+        {
+            Console.WriteLine("Test3");
+        }
+    }
+}
+
+    #pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+Console.WriteLine("skriv det tal som er bliver sagt");
+Console.ReadLine();
+
+
+
+
+/*
 // Creating a dictionary
 Dictionary<int, int> iplate = new Dictionary<int, int>();
 
 int userPlates;
-if (int.TryParse(userInput, out userPlates))
+if (int.TryParse(iplate, out userPlates))
 {
     if (userPlates >= 1 && userPlates <= 5)
 
@@ -34,3 +64,4 @@ if (int.TryParse(userInput, out userPlates))
         Console.WriteLine("something went wrong restart the program");
     }
 }
+*/
