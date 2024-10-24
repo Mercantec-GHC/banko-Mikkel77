19b02d3574b82fb09bfea8c10647d65082d6a862
Mikkel77 <Mikkelskovhansen70@gmail.com>
2024-10-24 21:05:44 +0200

    bankai project finished

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -1,106 +1,124 @@
-﻿int i = 1;
-//rember its a dictionary list 
-Dictionary<string, List<int>>iplate = new();
-iplate.Add("bankoplade1", new List<int>());
-while (true)
+﻿//for dictanary DONT DELETE
+using System;
+using System.Collections.Generic;
+
+class Program
 {
-    Console.WriteLine("skriv 15 tal fra pladen ind i format: første række og så den under og så den under, efter det skriv exit til at indtaste numer");
-    var bankoPlates = Console.ReadLine();
+    static void Main(string[] args)
+    {
+        // Dictionary for at gemme bankoplader husk list int
+        Dictionary<string, List<int>> bankoplate = new();
+        List<int> kaldteNumre = new();
+        int PlateT = 1;
+
+        
+        while (true)
+        {
+            Console.WriteLine($"Skriv de tal fra bankoplade {PlateT} ind i format: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 (husk mellemrum). Skriv 'exit' for at stoppe indtastning af plader:");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            string bankoPlates = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+            // exites bango pladerne 
+            if (bankoPlates?.ToLower() == "exit")
+                break;
+
+            string plateName = $"bankoplade{PlateT}";
+            bankoplate.Add(plateName, new List<int>());
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type. 
+            string[] numreInput = bankoPlates?.Split(" ");
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-
-    //check de første 5 10 og 15 numre i banko
-    if (int.TryParse(bankoPlates, out int number))
-    {
-        iplate["bankoplade1"].Add(number);
-
-        if (i == 5)
-        {
-            Console.WriteLine("test");
-            Console.ReadLine();
-
-        }
-        else if (i == 9)
-        {
-            Console.WriteLine("test2");
-            Console.ReadLine();
-        }
-        else if (i == 14)
-        {
-            Console.WriteLine("test3");
-            Console.ReadLine();
-        }
-        i++;
-
-    }
-    else
-    {
-        Console.WriteLine("intast et gyldigt nummer");
-    }
-
-    Console.WriteLine("De indtastede numre for 'bankoplade1' er:");
-    foreach (var num in iplate["bankoplade1"])
-    {
-        Console.WriteLine(num);
-    }
-
-    //exit'es the plate adder
-    if (bankoPlates?.ToLower() == "exit")
+            foreach (var tal in numreInput)
             {
-            Console.WriteLine("Plader gemt");
-            break;
-        }
-    #pragma warning restore CS8602 // Dereference of a possibly null reference.
-}
-
-Console.WriteLine("skriv det tal som bliver sagt");
-var navnetTal = Console.ReadLine();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// Creating a dictionary
-Dictionary<string, int> iplate = new Dictionary<string, int>();
-
-int userPlates;
-if (int.TryParse(iplate, out userPlates))
-{
-    if (userPlates >= 1 && userPlates <= 5)
-
-        for (int id = 1; id <= 5; id++)
-        {
-            if (id == userPlates)
-            {
-                Console.WriteLine($"du har valgt {userPlates} plader");
-                //WIP
-                iplate.Add(id, userPlates);
-                foreach (KeyValuePair<int, int> ele1 in iplate)
+                if (int.TryParse(tal, out int nummer))
                 {
-                    Console.WriteLine("{0} and {1}",
-                              ele1.Key, ele1.Value);
+                    bankoplate[plateName].Add(nummer);
                 }
-                Console.WriteLine();
+            }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+            Console.WriteLine($"'{plateName}' gemt");
+            PlateT++;
+        }
+
+        // nummer kald del
+        while (true)
+        {
+            Console.WriteLine("Indtast kaldt nummer (eller 'exit' for at afslutte):");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            string kaldInput = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+            if (kaldInput?.ToLower() == "exit")
+                break;
+
+            if (int.TryParse(kaldInput, out int kaldtNummer))
+            {
+                kaldteNumre.Add(kaldtNummer);
+
+                // sender dem til tjek hver plade for banko
+                foreach (var plade in bankoplate)
+                {
+                    TjekRækker(plade.Key, plade.Value, kaldteNumre);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Intast et gyldigt tal.");
             }
         }
-    else
+    }
+
+    // Tjekker om der er banko
+    static void TjekRækker(string pladeNavn, List<int> plade, List<int> kaldteNumre)
     {
-        Console.WriteLine("something went wrong restart the program");
+        int numrePerRække = 5;
+        bool[] rækkerMedBanko = new bool[3];
+
+        // Tjek hver række 
+        for (int række = 0; række < 3; række++)
+        {
+            int startrække = række * numrePerRække;
+            bool alleTalFundet = true;
+
+            for (int i = startrække; i < startrække + numrePerRække; i++)
+            {
+                if (!kaldteNumre.Contains(plade[i]))
+                {
+                    alleTalFundet = false;
+                    break;
+                }
+            }
+
+            if (alleTalFundet)
+            {
+                rækkerMedBanko[række] = true;
+            }
+        }
+
+        // Tæller banko
+        int antalRækkerMedBanko = 0;
+        foreach (bool banko in rækkerMedBanko)
+        {
+            if (banko)
+                antalRækkerMedBanko++;
+        }
+
+        // resultatetr for banko
+        if (antalRækkerMedBanko == 1)
+        {
+            Console.WriteLine($"{pladeNavn} har banko på 1 række!");
+        }
+        else if (antalRækkerMedBanko == 2)
+        {
+            Console.WriteLine($"{pladeNavn} har banko på 2 rækker!");
+        }
+        else if (antalRækkerMedBanko == 3)
+        {
+            Console.WriteLine($"{pladeNavn} har banko på 3 rækker!");
+        }
     }
 }
-*/
